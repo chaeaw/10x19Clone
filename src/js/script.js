fa9769d8e0670d90861a12cf731e988badfb4387
chaeaw04 <chaeaw04@gmail.com>
2023-08-11 21:09:53 +0900

    [#006 feat] thanks 추가
    - 한글 폰트 노토산스 추가
    - menu클릭시 thanks bg나타내기는 js로, 그 외는 모두 css 트랜지션으로 추가

--- src/js/script.js
+++ src/js/script.js
@@ -63,6 +63,60 @@
     }, 20);
   };
 
+  // click menu button
+  // menu button 클릭하면 bg가 덮어지고 밑에서 텍스트가 올라옴
+  // bg 크기 늘리기 & 텍스트 translateY or margin-bottom 이용
+  const $thanks = get(".thanks");
+  const $menu = get(".main__grid-menu");
+  const $menuButton = get(".main__grid-menu a");
+  const $thanksBg = get(".thanks__bg");
+  const $thanksClose = get(".thanks__close");
+  const $thanksLogo = get(".thanks__logo");
+  const $thanksCon = get(".thanks__con");
+
+  const bodyWidth = document.body.offsetWidth;
+  const bodyHeight = document.body.offsetHeight;
+  const buttonWidth = $menuButton.offsetWidth;
+  const buttonHeight = $menuButton.offsetHeight;
+  let menuWidth = $menu.offsetWidth;
+  let menuHeight = $menu.offsetHeight;
+
+  $thanksClose.style.width = menuWidth + "px";
+  $thanksClose.style.height = menuHeight + "px";
+  $thanksLogo.style.width = menuWidth + "px";
+  $thanksLogo.style.height = menuHeight + "px";
+
+  const coverBg = () => {
+    let bgWidth = (buttonWidth / bodyWidth) * 100;
+    let bgHeight = (buttonHeight / bodyHeight) * 100;
+    const fillBg = setInterval(() => {
+      bgWidth++;
+      bgHeight++;
+      $thanksBg.style.width = `${bgWidth}%`;
+      $thanksBg.style.height = `${bgHeight}%`;
+      if (bgWidth > 100 && bgHeight > 100) {
+        clearInterval(fillBg);
+      }
+    }, 1);
+  };
+
+  const uncoverBg = () => {
+    let bgWidth = 100;
+    let bgHeight = 100;
+    const disappearBg = setInterval(() => {
+      bgWidth--;
+      bgHeight--;
+      $thanksBg.style.width = `${bgWidth}%`;
+      $thanksBg.style.height = `${bgHeight}%`;
+      if (
+        bgWidth < (buttonWidth / bodyWidth) * 100 ||
+        bgHeight < (buttonHeight / bodyHeight) * 100
+      ) {
+        clearInterval(disappearBg);
+      }
+    }, 1);
+  };
+
   window.addEventListener("DOMContentLoaded", () => {
     $buttons.forEach((button) => {
       button.addEventListener("mouseenter", onEnterButton);
@@ -74,5 +128,25 @@
     });
 
     counterFn();
+  });
+  $menuButton.addEventListener("click", () => {
+    $thanks.classList.add("active");
+    coverBg();
+  });
+  $thanksClose.addEventListener("click", () => {
+    $thanks.classList.remove("active");
+    uncoverBg();
+  });
+  window.addEventListener("resize", () => {
+    setTimeout(function () {
+      menuWidth = $menu.offsetWidth;
+      menuHeight = $menu.offsetHeight;
+
+      console.log("resize event!");
+      $thanksClose.style.width = menuWidth + "px";
+      $thanksClose.style.height = menuHeight + "px";
+      $thanksLogo.style.width = menuWidth + "px";
+      $thanksLogo.style.height = menuHeight + "px";
+    }, 500);
   });
 })();
